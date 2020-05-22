709d270e5e73f7b9fb7a2fee5aa43e52fb503f2e
Tyler Bates <fondlytyler@gmail.com>
2020-05-22 10:45:07 -0600

    set up second vendor test.

--- VendorsPage.Tests/Models.Tests/VendorsTests.cs
+++ VendorsPage.Tests/Models.Tests/VendorsTests.cs
@@ -19,5 +19,18 @@ namespace VendorsPage.Tests
       Vendors newVendors = new Vendors("test vendor");
       Assert.AreEqual(typeof(Vendors), newVendors.GetType());
     }
+
+   [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Vendor";
+      Vendors newVendors = new Vendors(name);
+
+      string result = newVendors.Name;
+
+      Assert.AreEqual(name, result);
+    }
+  
+  
   }
 }
