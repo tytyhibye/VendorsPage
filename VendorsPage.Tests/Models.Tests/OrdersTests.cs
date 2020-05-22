f149c92d08a774b78ae7fc097dad2dc81c95e465
Tyler Bates <fondlytyler@gmail.com>
2020-05-22 11:47:38 -0600

    add first order test.

---
+++ VendorsPage.Tests/Models.Tests/OrdersTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorsPage.Models;
+
+namespace VendorsPage.Tests
+{
+  [TestClass]
+  public class OrdersTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Orders.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrdersConstructor_CreatesInstanceOfOrders_Orders()
+    {
+      Orders newOrder = new Orders("test");
+      Assert.AreEqual(typeof(Orders), newOrder.GetType());
+    }
+
+  }
+}
